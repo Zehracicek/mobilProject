ff663148931146dcfb008867feac2b764fd88452
Zehra Çiçek <121110580+Zehracicek@users.noreply.github.com>
2025-12-26 08:49:19 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\90536\\Desktop\\flutterl1l\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\90536\\Desktop\\mobilProject-main\\mobilProject-main" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\90536\\Desktop\\flutterl1l\\flutter"
+  "PROJECT_DIR=C:\\Users\\90536\\Desktop\\mobilProject-main\\mobilProject-main"
+  "FLUTTER_ROOT=C:\\Users\\90536\\Desktop\\flutterl1l\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\90536\\Desktop\\mobilProject-main\\mobilProject-main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\90536\\Desktop\\mobilProject-main\\mobilProject-main"
+  "FLUTTER_TARGET=C:\\Users\\90536\\Desktop\\mobilProject-main\\mobilProject-main\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguNA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NjZkZDkzZjlhMg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YTVjYjk2MzY5ZQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC4z"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\90536\\Desktop\\mobilProject-main\\mobilProject-main\\.dart_tool\\package_config.json"
+)
